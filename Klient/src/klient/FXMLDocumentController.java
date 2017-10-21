99def466ad50a3fdc6255cf71a4851fe28165fa8
kamilozo6 <s160829@student.pg.gda.pl>
2017-10-22 01:18:27 +0200

    credit i dodany game

--- Klient/src/klient/FXMLDocumentController.java
+++ Klient/src/klient/FXMLDocumentController.java
@@ -5,12 +5,18 @@
  */
 package klient;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 /**
  *
@@ -20,12 +26,41 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Label label;
+    @FXML
+    private Button credits;
+    @FXML
+    private Button back;
+    @FXML
+    private Button start;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    private void handleButtonAction(ActionEvent event)throws IOException {
+        Stage stage; 
+        Parent root;
+        if(event.getSource() == credits){
+        //get reference to the button's stage         
+            stage=(Stage) credits.getScene().getWindow();
+        //load up OTHER FXML document
+            root = FXMLLoader.load(getClass().getResource("Credits.fxml"));
+        }
+        else if (event.getSource() == back){
+            stage=(Stage) back.getScene().getWindow();
+            root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        }else if (event.getSource() == start)
+        {
+            stage=(Stage) start.getScene().getWindow();
+            root = FXMLLoader.load(getClass().getResource("Game.fxml"));
+        }
+        else
+        {
+            return;
+        }
+     //create a new scene with root and set the stage
+      Scene scene = new Scene(root);
+      stage.setScene(scene);
+      stage.show();
     }
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
